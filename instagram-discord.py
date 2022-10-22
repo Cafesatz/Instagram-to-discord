ec9e5c9e2590c109b42d8c2a5def72ec4d0911e7
Cafesatz <116442262+Cafesatz@users.noreply.github.com>
2022-10-22 15:43:57 +0200

    Update instagram-discord.py

--- instagram-discord.py
+++ instagram-discord.py
@@ -29,7 +29,7 @@ import time
 # Help: https://www.serverlab.ca/tutorials/linux/administration-linux/how-to-set-environment-variables-in-linux/
 
 INSTAGRAM_USERNAME = os.environ.get('cafesatzffm')
-webhook_url = os.environ.get('https://discordapp.com/api/webhooks/1033374042822021200/-kT5lTl5wfAYdpibFKT-Ol6ksTR26V23T_Lj1GFl8SUP4WttE_hw-3Knjt8eOwFcWzAM')
+WEBHOOK_URL = os.environ.get('https://discordapp.com/api/webhooks/1033374042822021200/-kT5lTl5wfAYdpibFKT-Ol6ksTR26V23T_Lj1GFl8SUP4WttE_hw-3Knjt8eOwFcWzAM')
 TIME_INTERVAL = os.environ.get('600')
 
 # ----------------------- Do not modify under this line ----------------------- #
